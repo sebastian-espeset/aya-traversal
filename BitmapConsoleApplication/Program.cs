9e9857402775b94fbc41746ddf03890f84653556
sebastian espeset <sebastian.c.espeset@gmail.com>
2023-03-22 13:25:51 -0500

    successfully importing image files

--- BitmapConsoleApplication/Program.cs
+++ BitmapConsoleApplication/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Drawing;
+Console.WriteLine("Hello, Bubblegum!");
+// import my images
+var bitmap1 = new Bitmap(@"C:\Users\sebas\OneDrive\Desktop\aya\bitmap-traversal\BitmapConsoleApplication\resources\Image01.bmp");
+var bitmap2 = new Bitmap(@"C:\Users\sebas\OneDrive\Desktop\aya\bitmap-traversal\BitmapConsoleApplication\resources\Image02.bmp");
+var bitmap3 = new Bitmap(@"C:\Users\sebas\OneDrive\Desktop\aya\bitmap-traversal\BitmapConsoleApplication\resources\Image03.bmp");
+
