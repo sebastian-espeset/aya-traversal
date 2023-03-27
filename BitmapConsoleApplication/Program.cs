dd6fb63658b40369dbfe4a2d65af4f64942f986b
sebastian espeset <sebastian.c.espeset@gmail.com>
2023-03-27 14:06:18 -0500

    javascript version updated

--- BitmapConsoleApplication/Program.cs
+++ BitmapConsoleApplication/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Drawing;
 Console.WriteLine("Hello, Bubblegum!");
+
 // import my images
 var bitmap1 = new Bitmap(@"C:\Users\sebas\OneDrive\Desktop\aya\bitmap-traversal\BitmapConsoleApplication\resources\Image01.bmp");
 var bitmap2 = new Bitmap(@"C:\Users\sebas\OneDrive\Desktop\aya\bitmap-traversal\BitmapConsoleApplication\resources\Image02.bmp");
---
+++ javascriptVersion.js
@@ -0,0 +1,48 @@
+
+let image = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 1, 1, 1, 0],
+  [0, 2, 2, 2, 0, 0, 1, 1, 1, 0],
+  [0, 2, 2, 2, 1, 1, 1, 1, 1, 0],
+  [0, 2, 2, 2, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+const allNeighbors=( matrix, column, row, value )=>{
+  let neighbors = [];
+  if(matrix[row][column+1]==1 && matrix[row][column-1]){
+    neighbors.push(true)
+  };
+  if(matrix[row+1][column]==value && matrix[row-1][column]){
+    neighbors.push(true)
+  };
+  if(matrix[row+1][column+1]==value && matrix[row-1][column+1]){
+    neighbors.push(true)
+  };
+  if(matrix[row-1][column-1]==value && matrix[row+1][column-1]){
+    neighbors.push(true)
+  };
+  if(neighbors.length==4){
+    return true
+  };
+  return false;
+};
+
+const centerFinder = (graph) => {
+  let centerCoords = [];
+  for( rows=0; rows < 10 ; rows++){
+    for(columns=0; columns < 10 ; columns++){
+      if(graph[rows][columns]==1){
+        if(allNeighbors(graph,columns,rows,1)){
+          centerCoords.push([columns,rows]);
+        };
+      };
+    };
+  };
+  console.log(centerCoords)
+  return centerCoords
+}
+centerFinder(image)
